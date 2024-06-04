d9ab51595982bff345faacbec0a33f91baf9fba6
Maxim280596 <max280596@gmail.com>
2024-06-04 19:20:58 +0300

    UPDATE || Deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,11 +3,14 @@ import { verifyContract } from "./verify";
 import { ButerinTower } from "../typechain-types";
 
 const MANAGER_ADDRESS = "0xF94AeE7BD5bdfc249746edF0C6Fc0F5E3c1DA226";
-const COINS_PRICE = ethers.utils.parseEther("0.0000001");
+const COINS_PRICE = ethers.utils.parseEther("0.000001");
 const startTimeUnix = "1";
 
 async function main() {
   const GAME_FACTORY = await ethers.getContractFactory("ButerinTower");
+  const NFT_COLLECTION_FACTORY = await ethers.getContractFactory(
+    "ButerinTowerErc1155"
+  );
   const game: ButerinTower = await GAME_FACTORY.deploy(
     startTimeUnix,
     MANAGER_ADDRESS,
@@ -16,11 +19,29 @@ async function main() {
   await game.deployTransaction.wait(5);
   console.log("Game deployed to:", game.address);
 
+  const collection = await NFT_COLLECTION_FACTORY.deploy(
+    "https://ipfs.io/ipfs/Qm",
+    MANAGER_ADDRESS,
+    game.address
+  );
+  await collection.deployTransaction.wait(5);
+  console.log("Collection deployed to:", collection.address);
+
   try {
     await verifyContract(game.address, [
       startTimeUnix,
       MANAGER_ADDRESS,
       COINS_PRICE,
+    ]);
+  } catch (error) {
+    console.error("Error verifying contract:", error);
+  }
+
+  try {
+    await verifyContract(collection.address, [
+      "https://ipfs.io/ipfs/Qm",
+      MANAGER_ADDRESS,
+      game.address,
     ]);
   } catch (error) {
     console.error("Error verifying contract:", error);
